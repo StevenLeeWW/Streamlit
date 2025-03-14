b90f12bef13c036ab5b2cf979a17296ab4e92685
Steven Lee WW <leewwsteven@gmail.com>
2025-03-14 09:39:53 +0800

    Update page1 and page2

--- pages/page1.py
+++ pages/page1.py
@@ -4,6 +4,4 @@ st.set_page_config(page_title="Page 1", page_icon=":shark:", layout="wide")
 
 st.sidebar.markdown("Page 1")
 
-st.markdown("# Page 1")
-st.markdown("## Header 2")
-st.markdown("### Header 3")
+st.subheader("# Page 1")
--- pages/page2.py
+++ pages/page2.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Page 2", page_icon=":shark:", layout="wide")
 
-st.markdown("# Page 2")
 st.sidebar.markdown("Page 2")
+st.markdown("# Page 2")
