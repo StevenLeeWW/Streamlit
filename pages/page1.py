be2a001835edd1062db117948719019f3dc80b59
Steven Lee WW <leewwsteven@gmail.com>
2025-03-14 09:31:42 +0800

    Add pages

---
+++ pages/page1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Page 1", page_icon=":shark:", layout="wide")
+
+st.sidebar.markdown("Page 1")
+
+st.markdown("# Page 1")
+st.markdown("## Header 2")
+st.markdown("### Header 3")
---
+++ pages/page2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Page 2", page_icon=":shark:", layout="wide")
+
+st.markdown("# Page 2")
+st.sidebar.markdown("Page 2")
